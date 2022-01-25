82a38e12535af4d49f5446e8a2363ed3fc400e8b
Joao Silva <joao.silva@diconium.com>
2022-01-25 12:31:03 +0000

    ref: another approach

--- count-manager/index.js
+++ count-manager/index.js
@@ -1,40 +1,41 @@
+class CountManager {
+    count = 0;
+    listeners = new Set();
+
+    plus() {
+        this.count += 1;
+        console.log(this.count);
+    }
+
+    minus() {
+        this.count -= 1;
+        console.log(this.count);
+    }
+
+    getCount() {
+        console.log(this.count);
+        return this.count;
+    }
+
+    subscribe(listener) {
+        this.listeners.add(listener);
+        console.log('subscribe[this]:', this);
+
+        return () => this.listeners.delete(listener);
+    }
+
+    notifyListeners() {
+        console.log('notifyListeners[this]:', this);
+        this.listeners.forEach((listener) => listener());
+    }
+}
+
 export const countManagerDefinition = {
     id: 'test:countmvc-count-manager',
 
     create: (env) => {
-        let count = 0;
-
         const v1 = (consumerId) => ({
-            featureService: {
-                plus() {
-                    count += 1;
-                    console.log(count);
-                },
-
-                minus() {
-                    count -= 1;
-                    console.log(count);
-                },
-
-                getCount() {
-                    console.log(count);
-                    return count;
-                },
-
-                subscribe(listener) {
-                    this.listeners.add(listener);
-                    console.log('subscribe:', this);
-
-                    return () => this.listeners.delete(listener);
-                },
-
-                listeners: new Set(),
-
-                notifyListeners() {
-                    console.log('notifyListeners:', this.listeners);
-                    this.listeners.forEach((listener) => listener());
-                }
-            },
+            featureService: new CountManager(),
         });
 
         return {
